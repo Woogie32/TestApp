d6181fe1bd3ee5214fd44570d218bd20542628a5
Woogie32 <woogie.0613@gmail.com>
2026-02-11 03:40:04 +0900

    Initial Commit

---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by woogie on 2/11/26.
+//
+
+import Testing
+@testable import TestApp
+
+struct TestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
